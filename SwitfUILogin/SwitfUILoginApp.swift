cf180076febe978883eb92f1c3cb205d90eeeba2
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-06-22 15:24:44 +0500

    Initial Commit

---
+++ SwitfUILogin/SwitfUILoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitfUILoginApp.swift
+//  SwitfUILogin
+//
+//  Created by Thor on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwitfUILoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
